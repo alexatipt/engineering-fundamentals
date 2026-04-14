da6881ffff4bebff6a3b58374353d591e1872f72
Alexander Wüthrich <alexander.wuethrich@ipt.ch>
2026-04-14 08:13:36 +0000

    add test action

--- src/__tests__/Counter.test.tsx
+++ src/__tests__/Counter.test.tsx
@@ -1,7 +1,12 @@
-import { render} from "@testing-library/react";
+import { render, screen, fireEvent} from "@testing-library/react";
 import Counter from "../Counter";
 
 test("increments count when button is clicked", () => {
     render(<Counter />);
-    // Add the test logic below
+  const button = screen.getByText(/count is 0/i);
+  expect(button).toBeInTheDocument();
+
+  fireEvent.click(button);
+
+  expect(screen.getByText(/count is 1/i)).toBeInTheDocument();
 });
